be01cb59e972c3bfa995b7a6238cadafc82292b0
Youssef Gamal-AbdelNasser <66358331+youssefgamal123@users.noreply.github.com>
2025-08-31 21:27:43 +0300

    modified base_2

--- Vjudge/Base2.cpp
+++ Vjudge/Base2.cpp
@@ -5,9 +5,9 @@ int main() {
     unsigned long long res = 0;
     for (int i = 0; i < 64; ++i) {
         int a;
-        if (!(cin >> a)) return 0; // safety
+        if (!(cin >> a)) return 0;
         if (a == 1) res |= (1ULL << i);
     }
-    cout << res << "\n";
+    cout << res << endl;
     return 0;
 }
